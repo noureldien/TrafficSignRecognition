26305463871518c5f0517887e92c85b926e4b6b5
Noureldien <nreldien@yahoo.com>
2015-08-03 12:32:04 +0100

    Commit

--- main.py
+++ main.py
@@ -140,11 +140,11 @@ print('Traffic Sign Recognition')
 
 # region Detection (Model 80)
 
-# img_dim_80 = 80
-# gtsrb_model_80 = 'D:\\_Dataset\\GTSRB\\cnn_model_80.pkl'
-# gtsdb_model_80 = 'D:\\_Dataset\\GTSDB\\las_model_80.pkl'
-# gtsdb_dataset_80 = 'D:\\_Dataset\\GTSDB\\gtsdb_prohibitory_organized_80.pkl'
-# gtsdb_dataset_conv_80 = 'D:\\_Dataset\\GTSDB\\gtsdb_prohibitory_convolved_80.pkl'
+img_dim_80 = 80
+gtsrb_model_80 = 'D:\\_Dataset\\GTSRB\\cnn_model_80.pkl'
+gtsdb_model_80 = 'D:\\_Dataset\\GTSDB\\las_model_80.pkl'
+gtsdb_dataset_80 = 'D:\\_Dataset\\GTSDB\\gtsdb_prohibitory_organized_80.pkl'
+gtsdb_dataset_conv_80 = 'D:\\_Dataset\\GTSDB\\gtsdb_prohibitory_convolved_80.pkl'
 
 # extract region images to train the detector
 # CNN.utils.serialize_gtsdb(img_dim_80, True)
@@ -162,8 +162,8 @@ print('Traffic Sign Recognition')
 # CNN.detec.detect_from_dataset(dataset_path=gtsdb_dataset_80, recognition_model_path=gtsrb_model_80, detection_model_path=gtsdb_model_80)
 
 # test the detector
-# CNN.detec.detect_img_from_file(img_path="D://_Dataset//GTSDB//Test_PNG//_img7.png", model_type=CNN.enums.ModelType._02_conv3_mlp2,
-#                               recognition_model_path=gtsrb_model_80, detection_model_path=gtsdb_model_80, img_dim=img_dim_80)
+CNN.detec.detect_img_from_file(img_path="D://_Dataset//GTSDB//Test_PNG//_img1.png", model_type=CNN.enums.ModelType._02_conv3_mlp2,
+                               recognition_model_path=gtsrb_model_80, detection_model_path=gtsdb_model_80, img_dim=img_dim_80)
 
 # endregion
 
